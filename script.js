f706f8c46178642986fbf88bdc25e6db6bbf0c79
shital-kamble <94334804+shital-kamble@users.noreply.github.com>
2023-03-23 10:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function calculateMinCost() {
-  //your code here
+var n =arr.length;
+	var sum=0;
+	for(var i=0; i<n; i++)
+		{
+			sum+=arr[i]+arr[i+1];
+		}
+	arr.sort();
+	System.out.println(sum);
   
   
   
